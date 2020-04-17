290212c6c969ea8e799ccebdd925ec8be447c74b
bhavyejain <bhavyej@gmail.com>
2020-04-17 16:55:21 +0530

    Add logout

--- dashboard/console/urls.py
+++ dashboard/console/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'console'
 urlpatterns = [
     path('', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/console/'), name='logout'),
     path('all/', views.index, name='index'),
     path('new/', views.new_requests, name='new-logs'),
     path('processing/', views.processing_requests, name='processing-logs'),
