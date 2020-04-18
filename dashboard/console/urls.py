fd2ca0884e557198d49e4fa7758931ab13decff5
bhavyejain <bhavyej@gmail.com>
2020-04-19 01:13:57 +0530

    remove testing urls and add urls for databse updation and data retrieval requests via AJAX

--- dashboard/console/urls.py
+++ dashboard/console/urls.py
@@ -7,7 +7,7 @@ app_name = 'console'
 urlpatterns = [
     path('', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/console/'), name='logout'),
-    path('all/', views.index, name='index'),
+    path('all/', views.all_requests, name='index'),
     path('new/', views.new_requests, name='new-logs'),
     path('processing/', views.processing_requests, name='processing-logs'),
     path('resolved/', views.resolved_requests, name='resolved-logs'),
@@ -15,6 +15,6 @@ urlpatterns = [
     path('update_status/<int:pk>/<str:status>/', views.update_status, name='update-status'),
     path('profile/', views.profile, name='profile'),
     path('analytics/<str:feature>/', views.analytics_view, name='analytics'),
-    path('test/', views.index_test, name='index_test'),
     path('sync_db/', views.sync_db, name='sync_db'),
+    path('get_logs/<str:status>/', views.get_logs, name='get_logs'),
 ]
